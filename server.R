173151df16533f49330fa30895910467d76da737
piklprado <piklprado@gmail.com>
2015-06-30 08:45:50 -0300

    Main plot changed to highlight rejection regions instead of extreme values

--- server.R
+++ server.R
@@ -161,12 +161,13 @@ shinyServer(function(input, output, session) {
               line <- svalue(); if(abs(line) > maxx) maxx = abs(line); 
               # draws the histogram
               oh <- hist(mydist, xlim=1.1*c(-maxx, maxx), main = "Distribution of the statistic of interest", col="skyblue", border="white", xlab="Statistic of interest")
-              # adds the extreme values in orange. the definition of "extreme" depends on whether the test is
+              # adds the rejection region in orange. The definition of the region depends on whether the test is
               # one sided or two sided
+              mydist.q <- quantile(mydist, probs=c(0.025, 0.05, 0.95, 0.975))
               mydist <- switch(input$pside,
-                              "Two sided" = mydist[abs(mydist) >= abs(line)],
-                              "Greater" = mydist[mydist >= line],
-                              "Lesser" = mydist[mydist <= line]
+                              "Two sided" = mydist[mydist < mydist.q[1] || mydist > mydist.q[4]],
+                              "Greater" = mydist[mydist > mydist.q[3]],
+                              "Lesser" = mydist[mydist < mydist.q[2]]
                               )
               if(length(mydist)>0) 
                 hist(mydist, xlim=1.1*c(-maxx,maxx), col="orange1", border="white", 

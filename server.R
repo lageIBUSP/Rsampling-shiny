1eb2c44d7ad8b1fcb3a9007a7e811c289c3e65aa
piklprado <piklprado@gmail.com>
2015-06-30 10:54:26 -0300

    still trying the rectangle ...

--- server.R
+++ server.R
@@ -180,7 +180,7 @@ shinyServer(function(input, output, session) {
                               "Greater" = c(mydist.q[3],1.1*maxx),
                               "Lesser" = c(1.1*-maxx, mydist.q[4])
                                     )
-                polygon(x=rejection, y=range(oh$counts), col=gray.colors(10, alpha=0.5)[1])
+                polygon(x=rep(rejection,2), y=rep(range(oh$counts), 2), col=gray.colors(10, alpha=0.5)[1])
             })
             ### simply displays the statistic of interest
             output$stat <- renderText({

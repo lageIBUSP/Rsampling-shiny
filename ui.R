587695427c872fe2dd98ecc338dc59d942822d80
piklprado <piklprado@gmail.com>
2015-07-10 12:40:58 -0300

    Separate conditional panel for F
    meandif and Fstatistic had teh same conditionalPanel, mixing their help messages. I created a separate panel for F.

--- ui.R
+++ ui.R
@@ -71,7 +71,7 @@ shinyUI(fluidPage(theme= "bootstrap.css",
                          choices=c("Column mean" = "smean",
                                    "Column standard deviation" = "ssd",
                              "Mean difference between 2 groups" = "meandif",
-                             "F-statistic for more than 2 groups" = "Fstatistic",
+                             "Variance ratio (F) for more than 2 groups" = "Fstatistic",
                                    "Mean difference between columns" = "meandifc",
                                    "Mean sum of rows" = "srow",
                                    "Mean sum of columns" = "scol",
@@ -137,12 +137,12 @@ shinyUI(fluidPage(theme= "bootstrap.css",
                  ),
              ### Panel for F-statistic:
              conditionalPanel(
-               helpText("The F-statistic function splits the data acording to a categorical variable. Then it calculates
-                           the ratio of among-group to within-group variances (F-statistic).
-                           Large differences between means of at least two groups lead to large values of F."),
-               selectInput("s1", "Categorical variable column: ", 1),
-               selectInput("s2", "Numerical variable column: ", 2),
-               condition="input.stat=='Fstatistic'"
+                 helpText("The variance ratio function splits the data acording to a categorical variable.
+                            Then it calculates the ratio of among-group to within-group variances, also known as Fisher's F.
+                            Large differences between means of at least two groups lead to large values of F."),
+                 selectInput("s1", "Categorical variable column: ", 1),
+                 selectInput("s2", "Numerical variable column: ", 2),
+                 condition="input.stat == 'Fstatistic'"
                  ),
              ### Panel for meandifc:
              conditionalPanel(

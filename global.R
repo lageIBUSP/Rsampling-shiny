32e27e767164c9a9ebe16fc3d79686dcdda2e193
Andre Chalom <andrechalom@gmail.com>
2015-07-31 03:03:34 -0300

    Finishing translations to pt-br

--- global.R
+++ global.R
@@ -1,22 +1,29 @@
 ##################################################
 ###       MULTILINGUAL VARIABLES AND FUNCTION  ###
 #################################################
-ifelse(exists("language"),lg <- language, lg <- "en")
-lang <- unique(read.csv('lang.csv', sep = ';', stringsAsFactors = FALSE, fileEncoding="UTF-8"))
-tr <- function(text, language, D = lang) {
+# The default language is "en". To use the translated version, set the language such as
+# > language <- "pt" 
+# before calling the app
+language <- ifelse(exists("language"), language, "en")
+dictionary <- read.csv('lang.csv', sep = ';', stringsAsFactors = FALSE, fileEncoding="UTF-8", header=TRUE)
+# If the language is not recognized, does not attempt to run the app. Stop immediately:
+if (! language %in% colnames(dictionary)[-1])
+  stop(paste0("Language is not recognized: ", language, "\n"))
+## Main translation function. Uses "language" and "dictionary" as globals, defined above
+# in this file. 
+tr <- function(text) {
     for (i in 1:length(text)) {
-      id <- which(D$value %in% text[i])
-      if (length(id) == 0) {
-        id <- which(D$en %in% text[i])
-        if (length(id) == 1) {
-          text[i] <- ifelse(language == "pt", D$pt[id], D$en[id])
-        } else if (length(id) > 1) {
-          print('Mulit-variable matched!')
-        }
-      } else if (length(id) == 1) {
-        text[i] <- ifelse(language == "pt", D$pt[id], D$en[id])
+      id <- which(dictionary$value %in% text[i])
+      # Trying again: 
+      if (length(id) == 0) 
+        id <- which(dictionary$en %in% text[i])
+      # If still no matches, prints a warning:
+      if (length(id) == 0) 
+        cat(paste('No matches for text:', text[i], '\n'))
+      if (length(id) == 1) {
+        text[i] <- ifelse(language == "pt", dictionary$pt[id], dictionary$en[id])
       } else if (length(id) > 1) {
-        print('Mulit-variable matched!')
+        cat(paste('More than one match for text:', text[i], '\n'))
       }
     }
     return(text)
